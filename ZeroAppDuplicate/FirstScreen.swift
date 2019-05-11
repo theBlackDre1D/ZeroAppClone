968ba9a79294c4bd35e5aec72e3f5df2ffc07f57
Michal Gaborik <michal.gaborik.77@gmail.com>
2019-05-11 16:56:43 +0200

    navigation controller is hidden

--- ZeroAppDuplicate/FirstScreen.swift
+++ ZeroAppDuplicate/FirstScreen.swift
@@ -10,28 +10,41 @@ import UIKit
 import AssetsLibrary
 
 class FirstScreen: UIViewController {
-    
-    let nextButton = UIButton()
-    
-    let logoImage: UIImageView = {
-        let imageView = UIImageView(image: #imageLiteral(resourceName: "logo"))
-        
+    let image: UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "opo_human"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        // Do any additional setup after loading the view.
+        view.backgroundColor = colorFromHex(rgbValue: 0xFCFAF7, alpha: 1)
+        view.addSubview(image)
+        
+        setupLayout()
     }
     
-    func setNextButtonConstrains() {
-        nextButton.translatesAutoresizingMaskIntoConstraints = false
-        nextButton.leadingAnchor.constraint(equalTo: view.leadingAnchor , constant: 20).isActive = true
-        nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
-        nextButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
-        nextButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 0).isActive = true
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.navigationBar.isHidden = true
     }
-
+    
+    func colorFromHex(rgbValue: UInt32, alpha: Double = 1.0) -> UIColor {
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
+        let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
+        let blue = CGFloat(rgbValue & 0xFF)/256.0
+        
+        return UIColor(red:red, green:green, blue:blue, alpha:CGFloat(alpha))
+    }
+    
+    private func setupLayout() {
+        setupImage()
+    }
+    
+    private func setupImage() {
+        image.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+        image.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        image.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        image.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
+        image.contentMode = .scaleAspectFit
+    }
 }
